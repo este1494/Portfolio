13703872fa2e1d177a345596801ede75f418b30f
Estefanía Macedo-Funder <estefunder@gmail.com>
2023-01-05 15:54:16 +0100

    Første upload

---
+++ burger-menu.js
@@ -0,0 +1,32 @@
+window.addEventListener("load", etFunktionsNavn);
+
+console.log("siden vises");
+
+function navigation() {
+  const nav = document.querySelector(".menu-navn");
+  const burger2 = document.querySelector(".menu2");
+  document.addEventListener("click", () => {
+    nav.classList.add("showMenu");
+  });
+}
+
+/* const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger = document.querySelector(".hamburger"); */
+/* const closeIcon = document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon"); */
+
+/* 
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+} */
+
+/* hamburger.addEventListener("click", toggleMenu); */
